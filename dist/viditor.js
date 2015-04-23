784f13bc9782205b303e15179bc3957f36cfe7c7
Andrew McPherson <andrewmcp333@gmail.com>
2015-04-22 20:35:58 -0400

    Hit the route for fusion on the server.

--- dist/viditor.js
+++ dist/viditor.js
@@ -3,15 +3,13 @@ var Reqwest = require("reqwest")
 var Bluebird = require("bluebird")
 
 window.Viditor = {
-	getYoutubeVideo: function() {
+	fusion: function(protovideo) {
 		return new Bluebird(function(resolve, reject) {
 			Reqwest({
 				"method": "POST",
 				"contentType": "application/json",
 				"url": "http://localhost:8080/v2/fusion",
-				"data": JSON.stringify({
-					"format": "mp4"
-				}),
+				"data": JSON.stringify(protovideo),
 				"type": "json"
 			})
 			.then(function(response) {
